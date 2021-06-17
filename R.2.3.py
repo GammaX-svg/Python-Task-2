748cee275c0b937efc591e550a0bc308148e7349
GammaX-svg <58239068+GammaX-svg@users.noreply.github.com>
2021-06-17 14:43:18 +0800

    Add files via upload

---
+++ R.2.3.py
@@ -0,0 +1,10 @@
+#Describe a component from a text-editor GUI and the methods that it encapsulates
+
+
+# Component: Document
+# Document would have an atrribute _content that would contain all the characters in the document.
+# Methods:
+#   1. copy(): 
+#   2. paste(): 
+#   3. insert():
+#   4. delete():
